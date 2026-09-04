b6eacafeb16b6a5686735a9ce9dae06890ae4aa7
wuyaos <wufeifeng_hust@163.com>
2026-09-05 02:12:24 +0800

    feat: PT_BatchDownload 适配 shadowthein.net/browse.php
    - DEFAULT_PATHS 加 /browse.php(NexusPHP 标准列表页), NexusPHPAdapter 自动适配
    - 0.6.5 -> 0.6.6

--- PT_BatchDownload.user.js
+++ PT_BatchDownload.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PT 批量下载种子
 // @namespace    https://github.com/wuyaos/greasyfork_scripts
-// @version      0.6.5
+// @version      0.6.6
 // @description  通用 PT 当前页批量下载工具，支持关键字/体积/做种数/优惠多选筛选、浏览器直下(zip打包)、qBittorrent/Transmission 推送。适配 NexusPHP、Unit3D(/torrents)、Gazelle(GGn) 列表页。
 // @author       wuyaos & AI
 // @match        https://*/*.php*
@@ -28,7 +28,7 @@
   const TOGGLE_ID = 'ptbd-toggle'
   const CUSTOM_SITES_KEY = 'ptbd_custom_sites'
   const DOWNLOADERS_KEY = 'ptbd_downloaders'
-  const DEFAULT_PATHS = ['/userdetails.php', '/torrents.php', '/special.php']
+  const DEFAULT_PATHS = ['/userdetails.php', '/torrents.php', '/special.php', '/browse.php']
   // Unit3D 架构站点列表页 pathname（RESTful 路由，区别于 NexusPHP 的 .php）
   const UNIT3D_LIST_PATH = '/torrents'
   const UNIT3D_DL_SELECTOR = 'a[href*="/torrents/download/"]'
